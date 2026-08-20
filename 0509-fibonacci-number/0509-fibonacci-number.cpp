0cddc9e821249d555bbbc041b80b2c762ffc3f2a
niharikagupta21022006-beep <niharikagupta21022006@gmail.com>
2026-08-20 19:18:38 +0530

    Time: 14 ms (22.57%), Space: 7.6 MB (88.88%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int fibonacci(int n){
+        if( n == 1){
+            return 1;
+        }
+
+        if(n == 0){
+            return 0;
+        }
+
+        else{
+            return (fibonacci(n-1) + fibonacci(n-2));
+        }
+    }
+    int fib(int n) {
+        return fibonacci(n);
+    }
+};
